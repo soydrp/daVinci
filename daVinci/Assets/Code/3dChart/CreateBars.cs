9f43a748d262621c7c80473fc6e519d3515a14ff
soydrp <david_ruizperret@merck.com>
2016-05-11 22:13:11 +0200

    orbiting

--- daVinci/Assets/Code/3dChart/CreateBars.cs
+++ daVinci/Assets/Code/3dChart/CreateBars.cs
@@ -14,6 +14,7 @@ public class CreateBars : MonoBehaviour {
 
     public string barType = "prefabs/bars/prefabSqBar"; //prefab name: can be bar, dot, ...
 
+
     void Start () {
 
         barSeparation = (float)Settings.ENV_MAX_SIZE_WIDTH / (float)JSONtoObj.MainChart.xaxis.values.Count;
@@ -32,6 +33,11 @@ public class CreateBars : MonoBehaviour {
                 //we used to store the bars in the jsontoobj array, but doesnt seem that interesting now. back to flat objects...
                 //JSONtoObj.MainChart.category[numCat].bars[numValues] = Instantiate(Resources.Load("prefabs/bars/prefabSqBar"), new Vector3(numValues * barSeparation, 0, (numCat * barSeparationZ) + (barSeparationZ / 2)), Quaternion.identity) as GameObject;
                 GameObject bar = Instantiate(Resources.Load(barType), new Vector3(numValues * barSeparation, 0, (numCat * barSeparationZ) + (barSeparationZ / 2)), Quaternion.identity) as GameObject;
+
+                //change bar color
+                //double red = JSONtoObj.MainChart.category[numCat].values[numValues] / 255;
+                //GameObject theBar = GameObject.Find("theBar"); //the cube within the bar prefab
+                //theBar.GetComponent<Renderer>().material.color = new Color(numCat * 20, (float)red, 1);
 
                 // thsi next section copies the relevant attributes of the bar in the bar itself, so they can be accessed later - currently in the create pop up label class
                 var barVal = bar.GetComponent<CreatePopUpLabel>();
